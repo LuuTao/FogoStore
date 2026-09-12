c4a2e50dbabbb58170f27441d4bfd87e6b78f9f3
LuuVanTao <127680365+LuuVanTao@users.noreply.github.com>
2026-09-12 14:17:45 +0700

    update code

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,14 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://fogo-store-api.onrender.com/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
